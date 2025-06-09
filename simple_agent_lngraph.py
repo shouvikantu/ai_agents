d1d0065827c612eb6fd762396f056cf949d01509
shouvikantu <shouvikwu26@gmail.com>
2025-06-09 18:29:05 +0600

    memory added

--- simple_agent_lngraph.py
+++ simple_agent_lngraph.py
@@ -5,11 +5,15 @@ from openai import OpenAI
 import re
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, END
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.sqlite import SqliteSaver
 
 load_dotenv()
 
 # pull your raw key...
 raw_key = os.getenv("OPENAI_API_KEY")
+tavily_api_key = os.getenv("TAVILY_API_KEY")
 if not raw_key:
     raise ValueError("OPENAI_API_KEY not set")
 
@@ -29,9 +33,16 @@ from langgraph.graph.message import add_messages
 class State(TypedDict):
     messages: Annotated[list, add_messages]
     
+tool = TavilySearchResults(max_results=2)
+# resp = tool.invoke("Who is shouvik ahmed antu?")
+# print(resp)
+tools = [tool]
+
+model_with_tools = model.bind_tools(tools)
+    
 def bot(state: State):
     print(state["messages"])
-    return {"messages" : [model.invoke(state["messages"])]}
+    return {"messages" : [model_with_tools.invoke(state["messages"])]}
 
 graph_builder = StateGraph(State)
 
@@ -39,19 +50,34 @@ graph_builder.add_node("bot", bot)
 
 graph_builder.set_entry_point("bot")
 
-graph_builder.set_finish_point("bot")
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
 
-graph = graph_builder.compile()
+graph_builder.add_conditional_edges("bot", tools_condition)
 
-# res = graph.invoke({"messages": ["Hello, How are you?"]})
+#MEMORY
+memory = SqliteSaver.from_conn_string(":memory:")
 
-# print(res)
 
-while True:
-    user_input = input("User: ")
-    if user_input in ["q", "quit", "exit"]:
-        print("goodbye!")
-        break
-    for event in graph.stream({"messages": ("user", user_input)}):
-        for value in event.values():
-            print("Assistant: ", value['messages'][-1].content)
+with SqliteSaver.from_conn_string(":memory:") as memory:
+    graph = graph_builder.compile(checkpointer=memory)
+
+    config = {
+        "configurable" : {"thread_id": 1}
+    }
+
+    user_input = "Hi I'm bond. Do u know what fruit I like?"
+
+    events = graph.stream({"messages": ("user", user_input)}, config, stream_mode="values")
+
+    for event in events:
+        event["messages"][-1].pretty_print()
+
+# while True:
+#     user_input = input("User: ")
+#     if user_input in ["q", "quit", "exit"]:
+#         print("goodbye!")
+#         break
+#     for event in graph.stream({"messages": ("user", user_input)}):
+#         for value in event.values():
+#             print("Assistant: ", value['messages'][-1].content)
